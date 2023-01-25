5c258523b1587e5f1f1413e4a9ccece4b3fe8000
mekatama <tamatte@hotmail.com>
2023-01-25 23:48:53 +0900

    改造したけど未使用になった

--- Scene1_GateIn.cs
+++ Scene1_GateIn.cs
@@ -5,11 +5,31 @@ using UnityEngine.UI;
 using MoreMountains.Tools;
 
 public class Scene1_GateIn : MonoBehaviour{
+	private float step_time;	//経過時間カウント用
+	private bool isLoadScene;	//
+
+	void Start(){
+		step_time = 0.0f;		//経過時間初期化
+		isLoadScene = false;
+	}
+
+	void Update(){
+		if(isLoadScene == true){
+			step_time += Time.deltaTime;	//経過時間をカウント
+			if(step_time >= 1.0f){
+				//ロード画面呼び出してScene遷移
+				MMSceneLoadingManager.LoadScene("Test01_Title");
+			}
+		}
+
+	}
+
 	//Gateに接触したらScene移動
 	void OnTriggerEnter2D(Collider2D other){
 		if (other.gameObject.tag == "Player"){
-			//ロード画面呼び出してScene遷移
-			MMSceneLoadingManager.LoadScene("Test01_Title");
+			isLoadScene = true;
+//			//ロード画面呼び出してScene遷移
+//			MMSceneLoadingManager.LoadScene("Test01_Title");
 		}
 	}
 }
